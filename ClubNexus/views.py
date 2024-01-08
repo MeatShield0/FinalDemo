af5c1a0b47d61e4122a771dbee3161d6b3cc2790
DASsukanna <sukanna.das@msds.christuniversity.in>
2024-01-08 12:07:11 +0530

    third commit on home page

--- ClubNexus/views.py
+++ ClubNexus/views.py
@@ -3,21 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 
-def home (request):
-    return render(request,"base.html")
-
-def home(request):
-    return render(request, "ClubNexus/home.html")
-
-def about(request):
-    return render(request, "ClubNexus/about.html")
-
-def contact(request):
-    return render(request, "ClubNexus/contact.html")
-
-def event(request):
-    return render(request, "ClubNexus/event.html")
-
-def services(request):
-    return render(request, "ClubNexus/services.html")
+def index(request):
+    return render(request,"ClubNexus/index.html")
 
--- clubhub/settings.py
+++ clubhub/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ClubNexus'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'clubhub.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -62,7 +63,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'clubhub'
             ],
         },
     },
