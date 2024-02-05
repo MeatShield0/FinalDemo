a740b264e61db96995a8198d988c5ad75b58a5c8
Parul Sharma <parul3kin@gmail.com>
2024-02-05 14:40:11 +0530

    added data

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard_with_pivot, name='dashboard_with_pivot'),
-    path('data', views.pivot_data, name='pivot_data'),
+    path('data', views.pivot_data1, name='pivot_data1'),
+    path('data', views.pivot_data2, name='pivot_data2'),
+    path('data', views.pivot_data3, name='pivot_data3'),
 ]
