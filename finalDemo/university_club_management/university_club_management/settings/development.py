f96f88343fdd670fb27f53bfd9fd6a70b816a384
L. Anthony Carvalho <144004767+MeatShield0@users.noreply.github.com>
2024-11-01 11:23:28 +0530

    Add files via upload
    Initial Skeleton

---
+++ finalDemo/university_club_management/university_club_management/settings/development.py
@@ -0,0 +1,5 @@
+from .base import *
+
+DEBUG = True
+ALLOWED_HOSTS = []
+# Any other development-specific settings, like an SQLite database, debug toolbar, etc.
---
+++ finalDemo/university_club_management/university_club_management/settings/production.py
@@ -0,0 +1,5 @@
+from .base import *
+
+DEBUG = False
+ALLOWED_HOSTS = ['your_production_domain.com']
+# Production-specific settings, like database configuration and security settings
